f8b8d12097dc0ff93b9a55e1a01ccd59372d8e18
dmdeh <131264106+dmdeh@users.noreply.github.com>
2024-06-04 19:22:13 +0900

    feat: post api 연동 및 api 생성
    Co-authored-by: planetBing <planetBing@users.noreply.github.com>
    Co-authored-by: 임승현 <96limshyun@users.noreply.github.com>

--- server/app.js
+++ server/app.js
@@ -1,7 +1,31 @@
 import express from "express";
+import mongoose from "mongoose";
+import pagesRouter from "./routes/pagesRoute.js";
+import PageSchema from "./model/pages.js";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
+
+const mongoURI =
+"mongodb+srv://96limshyun:vI1QcxipDQv7xLt6@cluster0.t4amrc9.mongodb.net/"; // env 이동예정
+
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("MongoDB connected");
+});
+
+app.use(express.json());
+// app.use(bodyParser.json());
+app.use("/", pagesRouter);
 
 app.get("/", (req, res) => {
   res.send("하이!");
