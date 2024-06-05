cae2b7d0a09563f38ca4b25d88de7ee1053a70eb
dmdeh <131264106+dmdeh@users.noreply.github.com>
2024-06-05 17:40:15 +0900

    refactor: mongodb 코드 분리

--- server/routes/pagesRoute.js
+++ server/routes/pagesRoute.js
@@ -1,38 +1,60 @@
 import express from "express";
-import mongoose from "mongoose";
-import PageSchema from "../model/pages.js";
-import Page from "../model/pages.js"
-// const Page = mongoose.model("Page", PageSchema);
+import Page from "../model/pages.js";
+
 const pagesRouter = express.Router();
 
 pagesRouter.get("/api/pages", async (req, res) => {
   try {
     // await Page.deleteMany({})
     const pagesList = await Page.find();
-    res.json(pagesList)
-  } catch(error) {
+    res.json(pagesList);
+  } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
 pagesRouter.post("/api/pages", async (req, res) => {
-  console.log(req.body)
+  console.log(req.body);
   try {
     const pageData = req.body;
-    const newPage = new Page(pageData); 
+    const newPage = new Page(pageData);
     await newPage.save();
     res.status(200).json({ message: "추가 성공", data: newPage });
-  } catch(error) {
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+pagesRouter.patch("/api/pages/:id", async (req, res) => {
+  const articleId = req.params.id;
+  const { title } = req.body;
+
+  if (typeof title !== "string") {
+    return res.status(400).json({ error: "Title must be a string" });
+  }
+
+  try {
+    const article = await Page.findById(articleId);
+
+    if (!article) {
+      return res.status(404).json({ error: "Article not found" });
+    }
+
+    article.title = title;
+    await article.save();
+
+    res.json({ message: "Title updated successfully", article });
+  } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
 // pagesRouter.delete("/api/pages", async (req, res) => {
 //   try {
-//     const pageData = req.b ody;
-//     const newPage = new Page(pageData); 
+//     const pageData = req.body;
+//     const newPage = new Page(pageData);
 //     await newPage.save();
-//     res.status(200).json({ message: "추가 성공", data: newPage });
+//     res.status(200).json({ message: "삭제 성공", data: newPage });
 //   } catch(error) {
 //     res.status(500).json({ message: error.message });
 //   }
