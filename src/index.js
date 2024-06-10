f27f7befa3192854c8efe43a92a4b644afec2811
melroh629 <jw.roh629@gmail.com>
2024-06-10 17:08:00 +0900

    feat: diff 알고리즘 적용 이후 다른 부분을 교체하기
    - 현재는 트리형태로만 가능

--- src/index.js
+++ src/index.js
@@ -1,3 +1,131 @@
-import { vm } from './main.js';
+import { vm, vm2 } from './main.js';
 
-console.log(vm);
+function diffByBfs(current, next) {
+	const queue = [{ current, next }];
+	const change = [];
+	while (queue.length > 0) {
+		const { current, next } = queue.shift();
+
+		if (current.children) {
+			for (let i = 0; i < current.children.length; i++) {
+				queue.push({
+					current: current.children[i],
+					next: next.children[i],
+				});
+			}
+			if (current.type !== next.type) {
+				change.push({
+					target: 'type',
+					key: current.key,
+					type: next.type,
+				});
+			}
+			if (current.props.id !== 'app' && current.props !== next.props) {
+				change.push({
+					target: 'props',
+					key: current.key,
+					props: next.props,
+				});
+			}
+			if (current.children !== next.children) {
+				change.push({
+					target: 'children',
+					key: current.key,
+					children: next.children,
+				});
+			}
+		}
+		return change;
+	}
+}
+
+function trackByKey(node, changes) {
+	const queue = [{ node, parent: null, index: null }];
+	while (queue.length > 0) {
+		const { node: current, parent, index } = queue.shift();
+		const apply = changes.find(item => item.key === current.key);
+
+		if (apply) {
+			switch (apply.target) {
+				case 'type':
+					current.type = apply.type;
+					break;
+				case 'props':
+					current.props = apply.props;
+					break;
+				case 'children':
+					current.children = apply.children;
+					break;
+			}
+			// 부모 노드의 children을 갱신
+			if (parent && index !== null) {
+				parent.children[index] = current;
+			}
+		}
+
+		if (current.children) {
+			for (let i = 0; i < current.children.length; i++) {
+				queue.push({ node: current.children[i], parent: current, index: i });
+			}
+		}
+	}
+	console.log(node);
+	return node;
+}
+
+function updateElement($parent, newNode, oldNode, index = 0) {
+	if (!oldNode) {
+		$parent.appendChild(createElement(newNode));
+	} else if (!newNode) {
+		$parent.removeChild($parent.childNodes[index]);
+	} else if (isNodeChanged(newNode, oldNode)) {
+		$parent.replaceChild(createElement(newNode), $parent.childNodes[index]);
+	} else if (newNode.type) {
+		const newLength = newNode.children.length;
+		const oldLength = oldNode.children.length;
+		for (let i = 0; i < newLength || i < oldLength; i++) {
+			updateElement(
+				$parent.childNodes[index],
+				newNode.children[i],
+				oldNode.children[i],
+				i
+			);
+		}
+	}
+}
+
+function isNodeChanged(node1, node2) {
+	return (
+		typeof node1 !== typeof node2 ||
+		(typeof node1 === 'string' && node1 !== node2) ||
+		node1.type !== node2.type
+	);
+}
+
+function createElement(node) {
+	if (typeof node === 'string') {
+		return document.createTextNode(node);
+	}
+	const $el = document.createElement(node.type);
+	if (node.props) {
+		Object.keys(node.props).forEach(key => {
+			$el.setAttribute(key, node.props[key]);
+		});
+	}
+	if (node.children) {
+		node.children.forEach(child => {
+			const $child = createElement(child);
+			$el.appendChild($child);
+		});
+	}
+	console.log('$el', $el);
+	return $el;
+}
+const compare = (current, next) => {
+	const change = diffByBfs(current, next);
+	const updatedNode = trackByKey(current, change);
+	const $app = document.getElementById('root');
+	updateElement($app, updatedNode, current);
+};
+
+compare(vm, vm2);
--- src/main.js
+++ src/main.js
@@ -1,31 +1,16 @@
 /** @jsx h */
 
-const key = Math.floor(Math.random());
-function h(type, props, ...children) {
+export function h(type, props, ...children) {
   return {
     type,
     props,
-    key: key,
+    key: Math.floor(Math.random() * 1000),
     children: children.flat()
   };
 }
 export const vm = h("div", {
   id: "app"
-}, h("ul", null, h("li", null, h("input", {
-  type: "checkbox",
-  class: "toggle"
-}), "todo list item 1", h("button", {
-  class: "remove"
-}, "\uC0AD\uC81C")), h("li", {
-  class: "completed"
-}, h("input", {
-  type: "checkbox",
-  class: "toggle",
-  checked: true
-}), "todo list item 2", h("button", {
-  class: "remove"
-}, "\uC0AD\uC81C"))), h("form", null, h("input", {
-  type: "text"
-}), h("button", {
-  type: "submit"
-}, "\uCD94\uAC00")));
+}, h("p", null, "\u2728\uCF58\uD150\uCE20 \uAC12\u2728"));
+export const vm2 = h("div", {
+  id: "app"
+}, h("p", null, "\u2728\uC5C5\uB370\uC774\uD2B8 \uAC12\u2728"));
