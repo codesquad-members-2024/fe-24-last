d36d9551d022cbd96770b256592cbd58e417d9d4
dmdeh <131264106+dmdeh@users.noreply.github.com>
2024-06-10 11:10:53 +0900

    design: console 꾸미기

--- client/src/pages/Pages.tsx
+++ client/src/pages/Pages.tsx
@@ -13,4 +13,26 @@ const Wrapper = styled.div`
   display: flex;
 `;
 
+console.log(`	
+nnnnnnnnnnnnnnnnnnnnnnnnnnnnnn	
+nnnn                         nnnn	
+nnnnnnn                          nnnn	
+nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn	
+nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn	
+nnnnnnnn                           nnn	
+nnnnnnnn    nnnnnnnn     nnnnnnn   nnn	
+nnnnnnnn      nnnnnnn      nnn     nnn	
+nnnnnnnn      nnnnnnnn     nnn     nnn	
+nnnnnnnn      nnnnnnnnnn   nnn     nnn	
+nnnnnnnn      nnn nnnnnnn  nnn     nnn	
+nnnnnnnn      nnn  nnnnnnnnnnn     nnn	
+nnnnnnnn      nnn   nnnnnnnnnn     nnn	
+nnnnnnnn      nnn     nnnnnnnn     nnn	
+nnnnnnnn      nnn      nnnnnnn     nnn	
+nnnnnnnn    nnnnnnnn    nnnnnn     nnn	  
+  nnnnnn                           nnn		
+    nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn		looking for a job!		  
+      nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn		https://github.com/dmdeh			 
+`);
+
 export default Pages;
