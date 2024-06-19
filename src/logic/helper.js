293d0ac6d605de4384e76fd05d2b707267877e2d
mi-hye <satge13@gmail.com>
2024-06-19 22:31:00 +0900

    feat: 각 컴포넌트의 요소에 컴포넌트 키 생성

--- src/logic/helper.js
+++ src/logic/helper.js
@@ -1,8 +1,22 @@
 function h(type, props, ...children) {
 	if (typeof type === "function") {
-		return type(props);
+		return addComponentKey(type(), type.name);
 	}
 	return { type, props, children: children.flat() };
+}
+
+function addComponentKey(component, componentKey) {
+	if (typeof component === "string") return;
+
+	if (component.props)
+		component.props.componentKey = componentKey;
+	else component.props = { componentKey };
+
+	if (component.children.length > 0)
+		for (const child of component.children) {
+			addComponentKey(child, componentKey);
+		}
+	return component;
 }
 
 export { h };
