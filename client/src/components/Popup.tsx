1d1d6798519e78dbe999c3a280b1d5fb432dfec6
dmdeh <131264106+dmdeh@users.noreply.github.com>
2024-06-27 17:45:55 +0900

    design: Popup 디자인 추가

--- client/src/components/Popup.tsx
+++ client/src/components/Popup.tsx
@@ -8,9 +8,15 @@ interface PopupProps {
 }
 
 const BLOCK_POPUP_LIST = [
-  { type: "h2", name: "제목1", desc: "섹션 제목(대)" },
-  { type: "h3", name: "제목2", desc: "섹션 제목(중)" },
-  { type: "h4", name: "제목3", desc: "섹션 제목(소)" },
+  { type: "h2", name: "제목1", desc: "섹션 제목(대)",
+    img: "https://www.notion.so/images/blocks/header.57a7576a.png",
+  },
+  { type: "h3", name: "제목2", desc: "섹션 제목(중)",
+    img: "https://www.notion.so/images/blocks/subheader.9aab4769.png",
+  },
+  { type: "h4", name: "제목3", desc: "섹션 제목(소)",
+    img: "https://www.notion.so/images/blocks/subsubheader.d0ed0bb3.png",
+  },
 ];
 
 const Popup: React.FC<PopupProps> = ({ position, onChangeBlockType }) => {
@@ -30,12 +36,18 @@ const Popup: React.FC<PopupProps> = ({ position, onChangeBlockType }) => {
   return (
     <PopupContainer style={{ top: position.top, left: position.left }}>
       <div>기본 블록</div>
-      <button onClick={handleNewPage}>페이지</button>
+      <PopupBox onClick={handleNewPage}>
+        <img src="https://www.notion.so/images/blocks/page.83b0bf31.png" />
+        <div>페이지</div>
+      </PopupBox>
       {BLOCK_POPUP_LIST.map((cur, index) => (
-        <button key={index} onClick={() => onChangeBlockType(cur.type)}>
-          <p>{cur.name}</p>
-          <p>{cur.desc}</p>
-        </button>
+        <PopupBox key={index} onClick={() => onChangeBlockType(cur.type)}>
+          <img src={cur.img} />
+          <div>
+            <p>{cur.name}</p>
+            <p>{cur.desc}</p>
+          </div>
+        </PopupBox>
       ))}
     </PopupContainer>
   );
@@ -56,11 +68,30 @@ const PopupContainer = styled.div`
   margin-top: 30px;
   div {
     font-size: small;
+    padding: 5px 10px;
   }
-  button {
-    border: none;
-    padding: 10px;
-    cursor: pointer;
+`;
+
+const PopupBox = styled.div`
+  display: flex;
+  align-items: center;
+  line-height: 120%;
+  user-select: none;
+  min-height: 45px;
+  border: none;
+  padding: 10px;
+  cursor: pointer;
+  img {
+    width: 45px;
+    display: block;
+    object-fit: cover;
+    border-radius: 4px;
+    background: white;
+    box-shadow: rgba(15, 15, 15, 0.1) 0px 0px 0px 1px;
+  }
+  &:hover {
+    background-color: #f3f3f3;
+    border-radius: 5px;
   }
 `;
 
